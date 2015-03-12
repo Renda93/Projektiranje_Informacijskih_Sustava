fc1bc984ea6298caf0f83186a5233092920044b1
Renda93 <nrendi00fesb.hr>
2015-03-12 10:54:18 +0100

    Initial Commit

---
+++ Projektiranje_Informacijskih_Sustava/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projektiranje_Informacijskih_Sustava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
